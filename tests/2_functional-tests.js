07711059cc910a89ea68a2d7fc38297fe5c404c8
GuRuGu <peter.k.nets@gmail.com>
2021-07-15 12:17:06 +0300

    Create functional tests for submitting an issue with all fields or only with required fields

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,49 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+suite("Functional Tests", () => {
+  test("Create an issue with every field: POST request to /api/issues/{project}", done => {
+    const issue = {
+      issue_title: "Issue 1",
+      issue_text: "There is an issue",
+      created_by: "Wunderwaffe",
+      assigned_to: "Milwakee",
+      status_text: "In progress",
+    };
+
+    chai
+      .request(server)
+      .post("/api/issues/123")
+      .send(issue)
+      .end((err, res) => {
+        assert.equal(201);
+        assert.equal(res.type, "application/json");
+        assert.isObject(res.body);
+        assert.nestedInclude(res.body, issue);
+
+        done();
+      });
+  });
+  test("Create an issue with only required fields: POST request to /api/issues/{project}", done => {
+    chai
+      .request(server)
+      .post("/api/issues/123")
+      .send({
+        issue_title: "Topchik Issue 1",
+        issue_text: "There is an issue with Topchik",
+        created_by: "Topchik",
+      })
+      .end((err, res) => {
+        assert.equal(201);
+        assert.equal(res.type, "application/json");
+        assert.isObject(res.body);
+        assert.nestedInclude(res.body, issue);
+
+        done();
+      });
+  });
 });
